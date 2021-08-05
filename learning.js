0ba27d04974e8fe1b8b46cbb4f5370ee5f5de9b5
Kayla Weng <kaylaweng103@gmail.com>
2021-08-04 20:10:02 -0400

    not sure

--- learning.js
+++ learning.js
@@ -1,4 +1,4 @@
 // const startbutton = document.getElementsByClassName("button1");
 function printhello(){
-    console.log("hello"); //write code that directs user to level 1 and beyond
+     //write code that directs user to level 1 and beyond
 }
