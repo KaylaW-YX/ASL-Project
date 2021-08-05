51da004ec401fd7e5e9e791dbae9e0a423a4317e
Kayla Weng <kaylaweng103@gmail.com>
2021-08-05 00:17:57 -0400

    added button to blue div

--- learning.js
+++ learning.js
@@ -1,4 +1,5 @@
 // const startbutton = document.getElementsByClassName("button1");
-function printhello(){
+function directPage(){
      //write code that directs user to level 1 and beyond
+     
 }
