d7d1b4631efbabcdf0c4a092f95f87b210662886
Kayla Weng <kaylaweng103@gmail.com>
2021-08-09 14:00:16 -0400

    Random image is displayedgit add . !

--- learning.js
+++ learning.js
@@ -1,12 +1,8 @@
 // const startbutton = document.getElementsByClassName("button1");
 function giveDifferentQuestionImg(){
-     //code for different questions
-     //step 1: get random character to display --> function getRandomCharacters() (random charac)
-     //setp 2: add to whatever value returned from randomCharacter() of .png (actual file name)
      var randomCharacter = getRandomCharacter(1);
-     // console.log(randomCharacter); testing completed. working!
-     //stpe 3: displaying the image from corresponding file name from previous step 
-     
+     console.log(randomCharacter); // testing--working!
+     document.canvas.src = randomCharacter;
      //step 4: display all of the four possible answer choices -- array shuffling 
           //step 4b: using loop to make it correspond to html 1,2.3.4 (innerhtml)
 }
@@ -26,6 +22,7 @@ var newArray = ["b","a","c","d"] //new shuffled values
 //displaying correct the correct answer from left to right (loop)
 // variable for storing the the image being shown
 
+//gets a random character
 function getRandomCharacter(length) {
      var randomResult = '';
      const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -33,7 +30,7 @@ function getRandomCharacter(length) {
      for ( var i = 0; i < length; i++ ) {
        randomResult += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return randomResult + ".png";
+    return "alphabetAndNum/"+ randomResult + ".png";
  }
  
 //  console.log(getRandomCharacter(1)); testing out code (works)
