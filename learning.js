49334f8ae59fb4311e3c2d2af798b9eb2403527c
Kayla Weng <kaylaweng103@gmail.com>
2021-08-09 23:04:53 -0400

    added content for learning and nav bar

--- learning.js
+++ learning.js
@@ -1,25 +1,16 @@
 // const startbutton = document.getElementsByClassName("button1");
 function giveDifferentQuestionImg(){
      var randomCharacter = getRandomCharacter(1);
-     console.log(randomCharacter); // testing--working!
+     // console.log(randomCharacter); // test
      document.canvas.src = randomCharacter;
      //step 4: display all of the four possible answer choices -- array shuffling 
           //step 4b: using loop to make it correspond to html 1,2.3.4 (innerhtml)
 }
-//displaying new questions and new possible answers 
-giveDifferentQuestionImg();
-// Make function that generates random letters (use charat), include numbers, add in append "png"
-//make all the file names lowercased
-
-//display new answer in same function 
-//loops --> populate answer spots 
-
-//array --> with all the answer (abcd),. 3/4 are random letters shuffle array
 
 var array = ["a","b","c","d"]; // a is correct, rest is incorrect(random) --> shuffle (Random)
           // 0    1    2   3 (RANDOM ORDERING)
 var newArray = ["b","a","c","d"] //new shuffled values
-//displaying correct the correct answer from left to right (loop)
+//displaying correct the correct answer from left to rig11ht (loop)
 // variable for storing the the image being shown
 
 //gets a random character
@@ -32,11 +23,3 @@ function getRandomCharacter(length) {
     }
     return "alphabetAndNum/"+ randomResult + ".png";
  }
- 
-//  console.log(getRandomCharacter(1)); testing out code (works)
-
-// var randomCharacter = 'b' + ".png";
-
-//when user clicks on one answer --> displaying the answer:
-
-
