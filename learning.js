489eb5fcb26da8f7e5345a9fda4818a9bab83a40
Kayla Weng <kaylaweng103@gmail.com>
2021-08-16 21:53:56 -0400

    done some javascript

--- learning.js
+++ learning.js
@@ -1,18 +1,4 @@
-// const startbutton = document.getElementsByClassName("button1");
-function giveDifferentQuestionImg(){
-     var randomCharacter = getRandomCharacter(1);
-     // console.log(randomCharacter); // test
-     document.canvas.src = randomCharacter;
-     //step 4: display all of the four possible answer choices -- array shuffling 
-          //step 4b: using loop to make it correspond to html 1,2.3.4 (innerhtml)
-}
-
-var array = ["a","b","c","d"]; // a is correct, rest is incorrect(random) --> shuffle (Random)
-          // 0    1    2   3 (RANDOM ORDERING)
-var newArray = ["b","a","c","d"] //new shuffled values
-//displaying correct the correct answer from left to rig11ht (loop)
-// variable for storing the the image being shown
-
+var randomCharacter = '';
 //gets a random character
 function getRandomCharacter(length) {
      var randomResult = '';
@@ -21,5 +7,79 @@ function getRandomCharacter(length) {
      for ( var i = 0; i < length; i++ ) {
        randomResult += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return "alphabetAndNum/"+ randomResult + ".png";
+    return randomResult;
  }
+
+//gives a random image displayed
+function giveDifferentQuestionImg(){
+     randomCharacter = getRandomCharacter(1);
+     // console.log(randomCharacter); // test
+     document.canvas.src = "alphabetAndNum/"+ randomCharacter + ".png";
+     // 3 different characters in 3 times 
+     var possibleAns = [getRandomCharacter(1), getRandomCharacter(1), getRandomCharacter(1), randomCharacter]; 
+     shuffleArr(possibleAns);
+     document.getElementById("ans1").innerHTML = possibleAns[0];
+     document.getElementById("ans2").innerHTML = possibleAns[1];
+     document.getElementById("ans3").innerHTML = possibleAns[2];
+     document.getElementById("ans4").innerHTML = possibleAns[3];
+     //  console.log(randomCharacter)
+}
+
+//shuffles array 
+function shuffleArr(array){
+     var currentIndex = array.length, randomIndex;
+     // While there remain elements to shuffle...
+     while (0 !== currentIndex) {
+       // Pick a remaining element...
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex--;
+       // And swap it with the current element.
+       [array[currentIndex], array[randomIndex]] = [
+         array[randomIndex], array[currentIndex]];
+     }
+     return array;
+}
+
+//checks for if the user answer is correct 
+function selectAnswer(event){
+     //need to know if character selected = randomcharacter var 
+     const selectedButton = event.target.innerHTML;
+     if (selectedButton == randomCharacter){
+          document.querySelector('button').classList.add('btn-correct');
+     } else {
+          alert("try again!");
+     }
+}
+
+
+function setStatusClass(element, correct){
+     clearStatusClass(element)
+     if (correct){
+          element.classList.add('btn-correct')
+     } else {
+          element.classList.add('wrong')
+     }
+}
+
+function clearStatusClass(element){
+     element.classList.remove('btn-correct');
+     element.classList.remove('btn-wrong');
+}
+
+//start button
+const startButton = document.getElementById('start-btn');
+
+const questionContainerElement = document.getElementById('question-container');
+
+startButton.addEventListener('click', startLesson)
+
+
+function startLesson(){
+     startButton.classList.add('hide')
+     questionContainerElement.classList.remove('hide')
+     giveDifferentQuestionImg();
+}
+
+function resetState(){
+     
+}
